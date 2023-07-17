da3b339c36e181970b1eccb5489527b51f5bd7a3
Rickard Hallerbäck <rickard.hallerback@epiroc.com>
2023-07-17 09:56:29 +0200

    lualsp

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,4 +1,7 @@
 :call mkdir(stdpath('config'), 'p')
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
+let mapleader=","
 source ~/.vimrc
+
+set clipboard+=unnamedplus
--- .config/nvim/lua/nvim-lspconfig.lua
+++ .config/nvim/lua/nvim-lspconfig.lua
@@ -32,7 +32,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- C/C++
 if vim.fn.executable('clangd') > 0 then
@@ -48,6 +48,31 @@ cmd = {
 on_attach = on_attach
 }
 end
+
+-- Lua
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 
 -- Rust
 local rt = require("rust-tools")
